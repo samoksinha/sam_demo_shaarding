2a62637698bdbabd10adfe4cc1b028eda53db381
Samok Prasad Sinha <samok.sinha@gmail.com>
2025-04-03 18:08:09 +0530

    DB sharding commit

--- src/main/java/com/sam/db/shard/read/write/split/sam_demo_sharding/SamDemoShardingApplication.java
+++ src/main/java/com/sam/db/shard/read/write/split/sam_demo_sharding/SamDemoShardingApplication.java
@@ -1,9 +1,11 @@
 package com.sam.db.shard.read.write.split.sam_demo_sharding;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SamDemoShardingApplication {
 
     public static void main(String[] args) {
